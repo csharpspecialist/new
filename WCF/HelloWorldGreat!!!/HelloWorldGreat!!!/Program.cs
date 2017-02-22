09f6be2cc6417085026d5b81c9c90689b3d7ce84
JAX <netspec1@hotmail.com>
2017-02-22 13:24:14 -0500

    created hello World Great and Rectangle  Console applications

---
+++ WCF/HelloWorldGreat!!!/HelloWorldGreat!!!/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using the line below aleviates the need to 
+// write Console everytime we write or read 
+//from the console!!!
+using static System.Console;
+ 
+
+namespace HelloWorldGreat___
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Do u like Sports");
+
+            string dog = ReadLine().ToUpper();
+
+            if (dog == "YES")
+            {
+                WriteLine($"Your answer of {dog} means that U R A Man's Man!!!");
+            }
+            else if (dog == "NO")
+            {
+                WriteLine($"Your answer of {dog} means that U R A Girlie Man!!!");
+            }
+            else
+            {
+                WriteLine($"We don't understand your answer of {dog}!!");
+            }
+            ReadLine();
+
+            WriteLine("What kind of car do you drive ");
+
+            string what = ReadLine().ToUpper();
+
+            string dude = (what == "INFINITI") ? "U R a Great person" : "U Don't know anything";
+
+            WriteLine(dude);
+
+            ReadLine();
+
+            Green();
+
+        }
+
+
+
+        public static void Green()
+        {
+            WriteLine("Today is the 2md day of ");
+            ReadKey();
+        }
+    }
+}
---
+++ WCF/RectangleApp/RectangleApp/Rectangle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using static System.Console;
+
+
+namespace RectangleApp
+{
+    class Rectangle
+    {
+
+        //variables
+        double length;
+        double width;
+
+        public void AcceptDetails()
+        {
+            length = 4.5;
+            width = 3.5;
+        }
+
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+
+
+        public void Display()
+        {
+            Console.WriteLine("Length : {0}", length);
+            Console.WriteLine("Width : {0}", width);
+            Console.WriteLine("Area : {0}", GetArea());
+            Console.ReadKey();
+
+        }
+            //Console.WriteLine("what's the best kind of sandwich");
+
+            //string words = Console.ReadLine();
+
+        
+
+
+        class ExecuteRectangle
+        {
+            static void Main(string[] args)
+
+            {
+
+                Rectangle show = new Rectangle();
+
+               
+                //show.GetArea();
+                show.AcceptDetails();
+                show.Display();
+                Console.ReadKey();
+
+
+                Console.WriteLine("what's the best kind of sandwich");
+
+                string words = Console.ReadLine();
+            }
+        }
+
+        public void Car()
+        {
+            Console.WriteLine("Do u like bowling??");
+
+            string bowl = Console.ReadLine();
+
+            Console.WriteLine($"Your answer was {bowl} means that u r a sissy!!!  a BIG SISSY");
+        }
+
+    }
+}
