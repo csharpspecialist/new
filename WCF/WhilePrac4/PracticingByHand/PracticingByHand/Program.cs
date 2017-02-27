fd1bf0b2e8945c33aad7c6dce22f1ca4615d993f
csharpspecialist <netspec1@hotmail.com>
2017-02-27 01:21:16 -0500

    completed the while guessing game by hand

---
+++ WCF/WhilePrac4/PracticingByHand/PracticingByHand/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace PracticingByHand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rgen = new Random();
+
+            int randNum = rgen.Next(1, 21);
+
+            int guess;
+
+            int guessNum = 1;
+
+
+            WriteLine("Guess the random # between 1 and 20");
+
+            guess = Convert.ToInt16(ReadLine());
+
+            while(randNum != guess)
+            {
+                WriteLine($"Your guess of {guess} is not the Random # \n Guess again");
+                guess = Convert.ToInt16(ReadLine());
+                guessNum++;
+
+
+            }
+            WriteLine($"You guess the random # of {randNum} Hooray!!! \n You are a winna!!!");
+            WriteLine($"You guessed {guessNum} times");
+            ReadKey();
+
+
+        }
+    }
+}
