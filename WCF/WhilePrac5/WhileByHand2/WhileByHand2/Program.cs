7d0e5bf288aabcb1aeec68468069c648f3a0003f
csharpspecialist <netspec1@hotmail.com>
2017-02-27 19:49:38 -0500

    successfully completed writing the guessing game without tryparse or parse

---
+++ WCF/WhilePrac5/WhileByHand2/WhileByHand2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace WhileByHand2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int newNum;
+
+            int guessCount = 1;
+
+            int mysteryNem = rnd.Next(1, 21);
+            WriteLine("You are Playing...Guess the mystery # \n Guess between 1 and 21");
+
+            newNum = Convert.ToInt16(ReadLine());
+
+            while(newNum != mysteryNem)
+            {
+                Console.WriteLine($"Your guess of {newNum} was not the correct Guess \n Guess again");
+                newNum = Convert.ToInt16(ReadLine());
+                guessCount++;
+                 
+            }
+            WriteLine($"U Guessed the mystery # of {mysteryNem}!! \n U R a Winna!!!");
+            WriteLine($"U Guessed {guessCount} Times");
+            ReadLine();
+
+
+
+        }
+    }
+}
