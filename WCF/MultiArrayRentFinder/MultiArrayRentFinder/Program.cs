231f4f55f2f136e919d7c616765b088629160b70
csharpspecialist <netspec1@hotmail.com>
2017-03-05 15:03:14 -0500

    completed the MultiArray Rent Finder project successfully

--- WCF/MultiArrayRentFinder/MultiArrayRentFinder/Program.cs
+++ WCF/MultiArrayRentFinder/MultiArrayRentFinder/Program.cs
@@ -11,7 +11,14 @@ namespace MultiArrayRentFinder
     {
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.ApartmentSearch();
+            
+        }
 
+
+        public void ApartmentSearch()
+        {
             int[,] rents = { { 400, 450, 510},
             { 500, 560,630},
                 { 625, 675, 710},
@@ -20,13 +27,34 @@ namespace MultiArrayRentFinder
 
             int floor;
             int bedrooms;
-            
+
+            Console.WriteLine("Enter the floor on which u wish to live");
+
+            floor = Convert.ToInt16(ReadLine());
+
+            Console.WriteLine("Enter the # of bedrooms you are looking for");
+
+            bedrooms = Convert.ToInt16(ReadLine());
+
+            Console.WriteLine($"The rent is {rents[floor, bedrooms]} \n for an Apartment on floor # " +
+                $"{floor} with {bedrooms} bedroom(s) \n");
 
 
+            WriteLine("Would you like to see the price for another Apartment \n  Y or N \n ");
+            string searchAgain = ReadLine().ToUpper();
 
-
-
+            if (searchAgain == "Y" || searchAgain == "YES")
+            {
+                ApartmentSearch();
+            }
+            else
+            {
+                Console.WriteLine("Thanks for shopping with us!!! \n Come Again Soon!!");
+                Console.WriteLine("Press any key to exit");
+                ReadKey();
+            }
 
         }
+
     }
 }
