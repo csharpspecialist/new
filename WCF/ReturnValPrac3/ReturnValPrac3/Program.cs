081eba0304861a7763d28acf713830c0c724d4e5
JAX <netspec1@hotmail.com>
2017-03-07 08:49:45 -0500

    worked on valprac 3

--- WCF/ReturnValPrac3/ReturnValPrac3/Program.cs
+++ WCF/ReturnValPrac3/ReturnValPrac3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace ReturnValPrac3
 {
@@ -10,6 +11,27 @@ namespace ReturnValPrac3
     {
         static void Main(string[] args)
         {
+
+            Program p = new Program();
+            Console.WriteLine("Enter the first # to add");
+
+            int num1 = Convert.ToInt16(ReadLine());
+
+            Console.WriteLine("Enter the 2nd # to add");
+
+            int num2 = Convert.ToInt16(ReadLine());
+
+            p.MethodAdd(num1, num2);
+
         }
+
+        public int MethodAdd(int val1, int val2)
+        {
+
+            int answ = val1 * val2;
+
+            return answ;
+        }
+
     }
 }
