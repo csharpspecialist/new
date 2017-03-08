11fdee8db30da6f43988265caaa65e25fc087b55
csharpspecialist <netspec1@hotmail.com>
2017-03-08 01:01:51 -0500

    saved the Guessing Games Int.tryparse2 this time.  Also completed another return Val prac project

--- WCF/GuessingGameIntTryParse2/GuessingGameIntTryParse2/Program.cs
+++ WCF/GuessingGameIntTryParse2/GuessingGameIntTryParse2/Program.cs
@@ -10,6 +10,26 @@ namespace GuessingGameIntTryParse2
     {
         static void Main(string[] args)
         {
+            Random rand = new Random();
+
+            int mystNum = rand.Next(1, 21);
+
+            int realGuess;
+
+            Console.WriteLine("Guess the magic # between 1 and 21");
+            string stringGuess = Console.ReadLine();
+
+           while(!int.TryParse(stringGuess ,out realGuess))
+            {
+                Console.WriteLine("We don't recognize your entry \n" +
+                    "Guess the magic # between 1 and 21");
+                stringGuess = Console.ReadLine();
+
+            }
+
+
+         
+
         }
     }
 }
---
+++ WCF/ReturnValPrac5/ReturnValPrac5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ReturnValPrac5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+
+            //int totalInt;
+            //Console.WriteLine("First # to multiply");
+
+            //int fNum = Convert.ToInt16(ReadLine());
+
+            //Console.WriteLine("2nd # to multiply");
+
+            //int secNum = Convert.ToInt16(ReadLine());
+
+            //totalInt = p.Multi(fNum, secNum);
+
+            //Console.WriteLine($"The total is {totalInt}");
+            //ReadKey();
+
+            int[] numeros = {10,12,45,66 };
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);
+
+            }
+            int strk = numeros.Sum();
+
+            Console.WriteLine($"The sum of the numeros array is {strk}");
+            ReadKey();
+        }
+
+        public int Multi(int numVal1, int numVal2)
+        {
+            int sum = numVal1 + numVal2;
+
+
+            return sum;
+        }
+    }
+}
