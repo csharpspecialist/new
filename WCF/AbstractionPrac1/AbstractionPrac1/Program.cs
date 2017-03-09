c70fce9fa3d44fe62162f6c252b9425b60f2f3e5
csharpspecialist <netspec1@hotmail.com>
2017-03-08 20:24:57 -0500

    success in practicing an abstraction project

---
+++ WCF/AbstractionPrac1/AbstractionPrac1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AbstractionPrac1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car speed = new Car();
+
+            WriteLine($"This is the mileage {speed.GetMileage()}");
+            ReadKey();
+
+
+            WriteLine($"This is the color {speed.GetColor()}");
+            ReadKey();
+
+            WriteLine($"Here is the formula {speed.StealFormula()}");
+            ReadKey();
+        }
+
+
+
+
+    }
+
+    public class Car
+    {
+        public int mileage = 85;
+
+        public string color = "Red";
+
+        private string formula = "a * b";
+        public string StealFormula()
+        {
+            return Form();
+        }
+
+        private string Form()
+        {
+            return formula;
+        }
+
+        public int GetMileage()
+        {
+            return mileage;
+        }
+
+        public string GetColor()
+        {
+            return color;
+        }
+
+        private string GetEngineFormula()
+        {
+            return formula;
+        }
+
+        public string DisplayEngFormula()
+        {
+            return GetEngineFormula();
+        }
+
+    }
+
+}
