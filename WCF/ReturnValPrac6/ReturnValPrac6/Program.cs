6e81dbbbc78b5e32b972fbda8c7539c0f15a1b7d
csharpspecialist <netspec1@hotmail.com>
2017-03-10 03:28:01 -0500

    completed both a return val and a abstract prac project

---
+++ WCF/ReturnValPrac6/ReturnValPrac6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ReturnValPrac6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Program p = new Program();
+
+            int total;
+            Console.WriteLine("Enter a first int val");
+
+            int val1 = Convert.ToInt16(ReadLine());
+
+            Console.WriteLine("Enter a 2nd int val");
+
+            int val2 = Convert.ToInt16(ReadLine());
+
+            p.Computer(val1, val2);
+
+            total = p.Computer(val2, val1);
+
+            Console.WriteLine($"The total amount is {total}");
+
+            ReadKey();
+
+        }
+
+        public int Computer(int fVal, int sVal)
+        {
+            int complete = fVal * sVal;
+            return complete;
+        }
+
+
+    }
+}
