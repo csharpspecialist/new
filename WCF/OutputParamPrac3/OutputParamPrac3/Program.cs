8ef0d8c7674517dac3c237263dd21154a6c0876e
csharpspecialist <netspec1@hotmail.com>
2017-03-16 18:55:42 -0400

    completed another output params practice project

---
+++ WCF/OutputParamPrac3/OutputParamPrac3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace OutputParamPrac3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            double uno;
+
+            double duo;
+
+             p.FindAns(out uno, out duo);
+        }
+
+
+        public void FindAns(out double first, out double second)
+        {
+            //you must get the values for the vars from the user inside of this method of
+            //fight with this for a long time...U HAVE BEEN WARNED!!!!!
+            Console.WriteLine("type in the first # to multiply");
+
+            first = Convert.ToInt16(ReadLine());
+
+            Console.WriteLine("type in the 2nd # to multiply");
+
+            second = Convert.ToInt16(ReadLine());
+
+
+            double total = 0;
+            double INT_RATE = .07;
+            double rate = 0;
+          
+
+          
+
+            total = first * second;
+            rate += total * INT_RATE;
+            Console.WriteLine($"The amount of the first value is {first} \n" 
+                + $"And the amount of the second value is {second} \n" + 
+                $"the total is {total} and the total amount of interest is ${rate}");
+
+            ReadLine();
+
+        }
+
+
+
+    }
+}
