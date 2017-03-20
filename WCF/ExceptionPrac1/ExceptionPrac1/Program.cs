2d274a64b69663a3b2a39f106e83dc18ab5acbaa
csharpspecialist <netspec1@hotmail.com>
2017-03-20 01:19:19 -0400

    created a new proj to test exception handling

---
+++ WCF/ExceptionPrac1/ExceptionPrac1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ExceptionPrac1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+          
+
+            //p.GetMpg1();
+
+            p.GetMpg2();
+
+        }
+
+        public void GetMpg1()
+        {
+            int milesGal;
+           
+            Console.WriteLine("Enter miles driven");
+            int milesDriven = Convert.ToInt16( ReadLine());
+
+            Console.WriteLine("Enter gallons of gas used");
+            int galsUsed = Convert.ToInt16(ReadLine());
+
+            
+            if (milesDriven != 0 || galsUsed != 0)
+            {
+               milesGal = milesDriven / galsUsed;
+            }
+            else
+            {
+                milesGal = 0;
+            }
+
+            Console.WriteLine(milesGal);
+            ReadLine();
+
+        }
+
+        public void GetMpg2()
+        {
+            int milesDriven;
+            int galsGas;
+            int mpg;
+            try
+            {
+                Console.WriteLine("Enter miles driven");
+                milesDriven = Convert.ToInt16(ReadLine());
+
+                Console.WriteLine("Enter gallons of gas used");
+                galsGas = Convert.ToInt16(ReadLine());
+                mpg = milesDriven / galsGas;
+
+            }
+
+            catch(Exception)
+            {
+                mpg = 0;
+                Console.WriteLine("You attempted to divide by zero");
+            }
+
+
+            //if (milesDriven != 0 || galsUsed != 0)
+            //{
+            //    milesGal = milesDriven / galsUsed;
+            //}
+            //else
+            //{
+            //    milesGal = 0;
+            //}
+
+            Console.WriteLine(mpg);
+            ReadLine();
+
+        }
+
+
+    }
+}
---
+++ WCF/ListPrac1/ListPrac1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using static System.Console;
+
+namespace ListPrac1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Automobile> inventory = new List<Automobile>();
+
+            Automobile car1 = new Automobile();
+
+            car1.AutoID = 100;
+            car1.Make = "Dodge";
+            car1.Model = "Charger";
+            car1.Miles = 32001;
+
+            Automobile car2 = new Automobile();
+
+            car2.AutoID = 200;
+            car2.Make = "Dodge";
+            car2.Model = "Dart";
+            car2.Miles = 39061;
+
+            inventory.Add(car1);
+            inventory.Add(car2);
+
+            Console.WriteLine($"The oldest car was build in {inventory[1]}");
+            foreach (Automobile item in inventory)
+            {
+                Console.WriteLine(item);
+                
+            }
+
+            ReadLine();
+
+
+        }
+    }
+
+    class Automobile
+    {
+        public int AutoID { get; set; }
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Miles { get; set; }
+
+
+
+    }
+}
