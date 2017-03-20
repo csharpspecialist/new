1132a9ea9088c05a9e760eb5c3d7dc2ec6077d8d
csharpspecialist <netspec1@hotmail.com>
2017-03-20 01:29:45 -0400

    cleaned up some spaces and added 2 more exception methods

--- WCF/ExceptionPrac1/ExceptionPrac1/Program.cs
+++ WCF/ExceptionPrac1/ExceptionPrac1/Program.cs
@@ -18,6 +18,7 @@ namespace ExceptionPrac1
             //p.GetMpg1();
 
             p.GetMpg2();
+            p.GetMpg3();
 
         }
 
@@ -31,7 +32,7 @@ namespace ExceptionPrac1
             Console.WriteLine("Enter gallons of gas used");
             int galsUsed = Convert.ToInt16(ReadLine());
 
-            
+            //this one uses an if statement to handle the div by 0 exception
             if (milesDriven != 0 || galsUsed != 0)
             {
                milesGal = milesDriven / galsUsed;
@@ -63,26 +64,41 @@ namespace ExceptionPrac1
             }
 
             catch(Exception)
-            {
+            {//this one uses the exception method w/o a letter after it above
                 mpg = 0;
                 Console.WriteLine("You attempted to divide by zero");
-            }
-
-
-            //if (milesDriven != 0 || galsUsed != 0)
-            //{
-            //    milesGal = milesDriven / galsUsed;
-            //}
-            //else
-            //{
-            //    milesGal = 0;
-            //}
+            }                  
 
             Console.WriteLine(mpg);
             ReadLine();
 
         }
 
+        public void GetMpg3()
+        {
+            int milesDriven;
+            int galsGas;
+            int mpg;
+            try
+            {
+                Console.WriteLine("Enter miles driven");
+                milesDriven = Convert.ToInt16(ReadLine());
 
+                Console.WriteLine("Enter gallons of gas used");
+                galsGas = Convert.ToInt16(ReadLine());
+                mpg = milesDriven / galsGas;
+
+            }
+
+            catch (Exception e)
+            {//this one uses the built in e.message 
+                mpg = 0;
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine(mpg);
+            ReadLine();
+
+        }
     }
 }
