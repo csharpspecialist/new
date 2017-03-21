09fb8aa90481020c396db37f40ddda5a9065207f
JAX <netspec1@hotmail.com>
2017-03-21 12:50:42 -0400

    working on exception projects

---
+++ WCF/ExceptionPrac2/ExceptionPrac2/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ExceptionPrac2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Program p = new Program();
+          //  p.GasException();
+            p.FirstTry();
+            Console.WriteLine("The program just kept going \n" +
+                "This is a good thing!!!");
+
+            ReadLine();
+        }
+
+        public void IntException()
+        {
+            //this one blows up on purpose!!!!!!
+            int answ;
+            int res;
+            int zero = 0;
+            Console.WriteLine("Enter an integer");
+            answ = Convert.ToInt16(ReadLine());
+
+            res = answ / zero;
+
+            Console.WriteLine($"The answer is {answ} ");
+
+
+        }
+
+
+        public void GasException()
+        {
+            int milesDriven;
+            int galsGas;
+            int mpg;
+
+            //this one blows up on purpose also!!!!!!
+
+            Console.WriteLine("Enter miles driven");
+                milesDriven = Convert.ToInt16(ReadLine());
+
+                Console.WriteLine("Enter gallons of gas used");
+                galsGas = Convert.ToInt16(ReadLine());
+                mpg = milesDriven / galsGas;                    
+
+            Console.WriteLine($"Your car gets {mpg} miles per gallon");
+            ReadLine();
+            
+
+        }
+
+
+        public void FirstTry()
+        {
+            int milesDriven;
+            int galsGas;
+            int mpg;
+
+            try
+            {
+
+                Console.WriteLine("Enter miles driven");
+                milesDriven = Convert.ToInt16(ReadLine());
+
+                Console.WriteLine("Enter gallons of gas used");
+                galsGas = Convert.ToInt16(ReadLine());
+                mpg = milesDriven / galsGas;
+
+            }
+
+            catch (Exception e)
+            {
+                mpg = 0;
+               
+                //below is a pre-written message 
+                // that says u tried to divide by zero!!
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine($"Your car gets {mpg} miles per gallon");
+            ReadLine();
+
+
+
+        }
+
+
+
+
+    }
+}
