2aa340d2f82fd8686282b3f93a5dc9837838f92f
JAX <netspec1@hotmail.com>
2017-06-09 15:43:56 -0400

    created a new Loop Array Sum project

---
+++ WCF/LoopArraySum/LoopArraySum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int[] arr = new int[] { 1, 2, 3, 5 ,9, 66 };
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+                        
+            int total = p.dude(arr);
+
+            Console.WriteLine(sum);
+            Console.WriteLine(total);
+            Console.ReadLine();                       
+            
+        }
+
+
+        public int dude (int [] smash)
+        {
+
+            int sumation = smash.Sum();
+
+            return (sumation);
+
+        }
+
+
+
+    }
+}
