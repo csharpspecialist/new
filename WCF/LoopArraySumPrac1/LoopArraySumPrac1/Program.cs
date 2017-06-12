cc53d943854fdd06a95029a0fbbe2d95f7355a27
JAX <netspec1@hotmail.com>
2017-06-12 09:38:04 -0400

    practicing looping arrays from job interview question

---
+++ WCF/LoopArraySumPrac1/LoopArraySumPrac1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopArraySumPrac1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            int[] whole = new[]  { 1, 10, 25, 33, 20 };
+
+            for (int i = 0; i < whole.Length; i++)
+            {
+
+                //  Console.WriteLine(whole[i]);
+                total += (whole[i]);
+               // Console.WriteLine(total);
+            }
+
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+    }
+}
