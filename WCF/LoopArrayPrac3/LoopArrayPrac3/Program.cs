b83d8ae443dfb1b00057da7e4a29c32529ac8caf
JAX <netspec1@hotmail.com>
2017-06-16 11:56:16 -0400

    more practice with array sum

---
+++ WCF/LoopArrayPrac3/LoopArrayPrac3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopArrayPrac3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] nums = { 22, 25, 77, 13, 51 };
+
+            for(int d = 0; d < nums.Length; d++)
+            {
+                Console.WriteLine(nums[d]);
+
+                sum += nums[d];
+            }
+            Console.WriteLine($"The value of the array is {sum}");
+
+            Console.ReadKey();
+        }
+
+
+
+
+    }
+}
