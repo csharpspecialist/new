d23f090929ce794edc41881e5174aefecf1213c3
JAX <netspec1@hotmail.com>
2017-06-16 11:02:00 -0400

    practicing diff types of built in loops

---
+++ WCF/LoopArrayPrac2/LoopArrayPrac2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopArrayPrac2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = {1,15,8,320,14,6 };
+
+
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                Array.Reverse(numArray);
+                Console.WriteLine(numArray[i]);
+                 
+
+            }
+            Console.ReadKey();
+        }
+
+        public void Show(int [] numbers)
+        {
+
+        }
+
+    }
+}
