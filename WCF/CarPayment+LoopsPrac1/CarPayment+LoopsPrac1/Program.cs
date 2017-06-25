584127c1ad5cb8203a41dd8e40418b0660ed5cd4
csharpspecialist <netspec1@hotmail.com>
2017-06-25 00:10:51 -0400

    added different way to create an array method

--- WCF/CarPayment+LoopsPrac1/CarPayment+LoopsPrac1/Program.cs
+++ WCF/CarPayment+LoopsPrac1/CarPayment+LoopsPrac1/Program.cs
@@ -21,6 +21,7 @@ namespace CarPayment_LoopsPrac1
 
         private static void LoopTotal()
         {
+         
             int total = 0;
 
             for (int i = 2; i <= 20; i += 2)
@@ -66,13 +67,47 @@ namespace CarPayment_LoopsPrac1
             for (int year = 0; year < 10; year++)
             {
                 amount = total * ((decimal)Math.Pow(1.0 + rate, year));
-                Console.WriteLine("{0,4}{1,20:C}", year,amount);
+                Console.WriteLine("{0,4}{1,20:C}", year, amount);
             }
             ReadLine();
 
-
+            StringArray();
 
         }
+
+        private static void StringArray()
+        {
+            string[] friendNames = { "Todd Anthony", "Kevin Holton", "Shane Laigle" };
+            //int i;
+            WriteLine($"Here are {friendNames.Length} of my friends:");
+            for (int i = 0; i < friendNames.Length; i++)
+            {
+                WriteLine(friendNames[i]);
+            }
+            ReadKey();
+            ArrayListing();
+        }
+
+
+        public static void ArrayListing()
+        {
+
+            string[] dude = new string[6];
+            dude[0] = "Opel";
+            dude[1] = "BMW";
+            dude[2] = "Lexus";
+            dude[3] = "Chevy";
+            dude[4] = "Mercury";
+            dude[5] = "Lincoln";
+
+
+            for (int i = 0; i < dude.Length; i++)
+            {
+                Console.WriteLine(dude[i]);
+            }
+            Console.ReadLine();
+        }
+
 
 
     }
