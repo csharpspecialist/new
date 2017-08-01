29fbb2d608abbeb230494a8cd5d38835686117ac
RAIDEN <RAIDEN@DEBO>
2017-08-01 13:39:54 -0400

    completed Abstract Practice App 2 class project

---
+++ WCF/AbstractPracticeApp2/AbstractPracticeApp2/Hero.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPracticeApp2
+{
+   abstract class Hero
+    {
+
+        public abstract string Justice();       
+
+    }
+    
+    class Marvel : Hero
+    {
+        public int payment = 20;
+        private int equation = 5;
+
+        public override string Justice()
+        {
+            string muscle = "power";
+            return muscle;
+        }
+
+        private int Hulk()
+        {
+            equation += payment;
+            return equation;
+
+        }
+
+        public int Scheme()
+        {
+            return Hulk();
+        }
+
+
+
+    
+    }
+
+
+
+
+}
---
+++ WCF/AbstractPracticeApp2/AbstractPracticeApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPracticeApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Marvel spidey = new Marvel();
+
+            int bravery;
+            string speed;
+
+            speed = spidey.Justice();
+            Console.WriteLine(speed);
+
+
+            bravery = spidey.Scheme();
+
+            Console.WriteLine(bravery);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
