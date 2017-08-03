a43bc4adcc3045b70871b18bb2c97c43d1c43e37
csharpspecialist <netspec1@hotmail.com>
2017-08-03 01:35:29 -0400

    comp destructor & constructor app for class W comments

--- WCF/ConstructorPrac1/ConstructorPrac1/Line.cs
+++ WCF/ConstructorPrac1/ConstructorPrac1/Line.cs
@@ -28,15 +28,44 @@ namespace ConstructorPrac1
             return length;
         }
 
+        public static string GetData()
+        {
+            return "Dude...Whatever";
+            
+
+        }
+
+  
 
         static void Main(string[] args)
         {
+            string dw = GetData();
+            Console.WriteLine(dw);
             Line game = new Line(); //the constructor is called right here!!!!!
             //before anything else!!!!!
             game.SetLength(6.0);
             WriteLine($"Length of the line is {game.GetLength()}");
             WriteLine($"Length of the line is {game.length}");
+
+            Console.ReadLine();
+            Console.WriteLine("");
             ReadKey();
         }
+
+        /*only 1 destructor per class.
+         * they are used to destroy instances of classes
+         * they are only used with classes.
+         * they cannot be inherited or overloaded
+         * they cannot be called nor invoked. invoking is done automatically.
+         * they cannot take modifiers or have parameters.
+         * start with the ~ (tilda), must match the name of the class being destroyed
+         * demonstration below
+         * 
+         * */
+        ~Line()
+        {
+            System.Diagnostics.Trace.WriteLine("We are cleaning up our resources now!!!!");
+        }
+
     }
 }
