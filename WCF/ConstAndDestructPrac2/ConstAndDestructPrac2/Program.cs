76908be8dc2ccf51c182facd9af4070e550b4ce1
csharpspecialist <netspec1@hotmail.com>
2017-08-03 20:48:30 -0400

    comp Interface Prac1 + Props & Fields Prac 1. Working on Constr & Destruct 2

---
+++ WCF/ConstAndDestructPrac2/ConstAndDestructPrac2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstAndDestructPrac2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("Dr.", "Strange");
+            s.PrintFullName();
+
+            Console.WriteLine($"I like chili!!!");
+
+            Console.ReadLine();
+
+          
+        }
+    }
+}
---
+++ WCF/ConstAndDestructPrac2/ConstAndDestructPrac2/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstAndDestructPrac2
+{
+    class Student
+    {
+        //these must be fields!!!!! not Properties
+        string fName;
+        string lName;
+        
+        /* the constructor cannot have a return type!!!!!
+         * it is initializing the fields above*/
+        public Student(string Firstname, string LastName)
+        {
+            this.fName = Firstname;
+            this.lName = LastName;
+
+        }
+
+
+        public void PrintFullName()
+        {
+
+            Console.WriteLine($"My first name is {fName} and my last name is {lName}.");
+
+        }
+    }
+}
