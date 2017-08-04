f037df81a200bab8ab354469253932ad263b4c5f
RAIDEN <RAIDEN@DEBO>
2017-08-04 16:29:11 -0400

    comp Const & Destr Prac 2. Fixed List Prac 1 & List Prac 3

---
+++ WCF/ListPrac1/ListPrac1/Automobile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListPrac1
+{
+    class Automobile
+    {
+        public int AutoID { get; set; }
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Miles { get; set; }
+
+        public void PrintList()
+        {
+
+
+            Console.WriteLine($"Automobile Id # {AutoID} - {Year} {Make}, {Model} with {Miles} miles.");
+        }
+
+    }
+}
