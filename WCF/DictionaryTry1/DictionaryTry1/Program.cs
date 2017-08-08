9c4ce446c6bb16a10041de7bf7ab166ed5c9d92c
RAIDEN <RAIDEN@DEBO>
2017-08-08 12:25:59 -0400

    comp Dictionary Try 1 in class proj

--- WCF/DictionaryTry1/DictionaryTry1/Program.cs
+++ WCF/DictionaryTry1/DictionaryTry1/Program.cs
@@ -11,7 +11,7 @@ namespace DictionaryTry1
     {
         static void Main(string[] args)
         {
-            DebugEight4.EightFour();
+           
 
             Dictionary<string, int> animals = new Dictionary<string, int>();
             
