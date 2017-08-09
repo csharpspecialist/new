7ed7e1ca034d23cbd29be2020833ce2745679206
csharpspecialist <netspec1@hotmail.com>
2017-08-09 02:56:04 -0400

    cr Experiment 1 for testing purposes, added a proj file to it and linked to it for class demo

---
+++ WCF/Experiment1/Antidote/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antidote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("We R Ready to rock right now!!!");
+
+
+        }
+
+        public void Crush()
+        {
+
+            Console.WriteLine("We have smash and crash..Present and accounted for");
+
+        }
+
+
+    }
+
+    public class Durango //must be public or experiment proj will not see it!!!
+    {
+
+        public void Boxer()
+        {
+            Console.WriteLine("We Work to be the best that we can be");
+
+        }
+
+
+
+    }
+
+
+}
