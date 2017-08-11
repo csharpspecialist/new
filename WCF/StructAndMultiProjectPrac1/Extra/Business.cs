52338e1dc9b000445dc7bb190dcf0b2f908d7557
csharpspecialist <netspec1@hotmail.com>
2017-08-10 21:05:34 -0400

    comp Struct & Multi Class proj

---
+++ WCF/StructAndMultiProjectPrac1/Extra/Business.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StructAndMultiProjectPrac1;
+
+namespace Extra
+{
+    class Business : Master
+    {
+
+        Master m1 = new Master
+        {
+            ID = 200,
+            Name = "Bolo"
+
+        };
+
+     
+        
+
+
+        //add reference to StructAndMultiPrac Project file
+        static void Main()
+        {
+            //must be in a method to create/call a method from the other project!!!!! REMEMBER THIS!!!!
+            Master m = new Master();
+            m.Working();
+
+
+
+        }
+    }
+}
